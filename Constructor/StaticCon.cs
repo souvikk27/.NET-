3c31928b8032d69d1f9f537ba1cd9f9014ca82a7
SOUVIK <souvikvip@outlook.vom>
2022-08-23 10:41:39 +0530

    static constructor

---
+++ Constructor/StaticCon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Car
+    { 
+        // static constructor
+        static Car()
+        {
+            Console.WriteLine("Static Constructor");
+        }
+        // parameterless constructor
+        Car()
+        {
+            Console.WriteLine("Default Constructor");
+        }
+        static void Main(string[] args)
+        {
+            // call parameterless constructor
+            Car car1 = new Car();
+            // call parameterless constructor again
+            Car car2 = new Car();
+            Console.ReadLine();
+        }
+    }
+}
