e23d615bc52191ea1c5a9e43a5cd4821007d10e6
SOUVIK <souvikvip@outlook.vom>
2022-08-23 10:47:17 +0530

    private constructor

---
+++ Constructor/PrivateCon.cs
@@ -0,0 +1,20 @@
+﻿namespace Constructor
+{
+    class PrivateCon
+    {
+        // private constructor
+        private PrivateCon()
+        {
+            Console.WriteLine("Private Constructor");
+        }
+    }
+    class SecondClass
+    {
+        static void Main(string[] args)
+        {
+            // call private constructor
+            PrivateCon car1 = new PrivateCon();
+            Console.ReadLine();
+        }
+    }
+}
