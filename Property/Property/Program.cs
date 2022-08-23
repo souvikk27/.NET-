e114aec62edfe6eaa2c2ea3146109bc62163c18b
Souvik Kundu <85146980+souvikk27@users.noreply.github.com>
2022-08-23 11:24:59 +0530

    Add files via upload
    properties

---
+++ Property/Property/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Property
+{
+    class PropertyExample
+    {
+        private string name;
+        public string Name
+        {
+            get { 
+                return name; 
+            }
+            set { 
+                name = value; 
+            }
+        }
+        public static void Main(string[] args)
+        {
+            var prop = new PropertyExample();
+            prop.Name = "Souvik";
+            Console.WriteLine(prop.Name);
+            Console.ReadLine();
+        }
+    }
+}
