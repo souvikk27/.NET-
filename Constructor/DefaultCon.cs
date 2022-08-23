33e2aac77043e3b5da5e22b0bf22c7a0179dc17e
SOUVIK <SOUVIK@DESKTOP-2V3Q9JB>
2022-08-23 10:35:17 +0530

    Add project files.

---
+++ Constructor/DefaultCon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class DefaultCon
+    {
+        int number;
+       public static void Main(string[] args)
+        {
+            DefaultCon con = new DefaultCon();
+            Console.WriteLine("The value of number is " + con.number);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Constructor
+{
+    public class Con
+    {
+        int someNumber;
+        public Con(int num)
+        {
+            someNumber = num;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter random Number ");
+            int someNumber = int.Parse(Console.ReadLine());
+            Con x = new(someNumber);
+            Console.WriteLine("This Number " + x.someNumber);
+        }
+    }
+}
