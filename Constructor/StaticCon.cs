a7a150ae263f83d14225206915772706305f51ec
SOUVIK <souvikvip@outlook.vom>
2022-08-23 10:43:14 +0530

    static constructor

--- Constructor/StaticCon.cs
+++ Constructor/StaticCon.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Constructor
+﻿namespace Constructor
 {
     class Car
-    { 
+    {
         // static constructor
         static Car()
         {
