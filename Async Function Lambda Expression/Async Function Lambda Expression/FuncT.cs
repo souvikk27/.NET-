1fd42a0d5f537eb21d2e7f44e503918e9cf800f4
Souvik Kundu <85146980+souvikk27@users.noreply.github.com>
2022-08-24 16:17:00 +0530

    Add files via upload

---
+++ Async Function Lambda Expression/Async Function Lambda Expression/FuncT.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Async_Function_Lambda_Expression
+{
+    class Example
+    {
+        public static void Main(string[] args)
+        {
+            Func<int, int, int> multi = (x, y) => { return x * y; };
+            Console.WriteLine(multi(5, 2));
+        }
+    }
+}
---
+++ Async Function Lambda Expression/Async Function Lambda Expression/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //Action debug = () => System.Diagnostics.Debug.WriteLine("Expression Lambda");
+        //debug();
+        Action<string> debug = (s) => System.Diagnostics.Debug.WriteLine(s);
+        debug("Expression Lambda with assigned vriable");
+
+        Action statement = () =>
+        {
+            System.Diagnostics.Debug.WriteLine("Statement Lambda!");
+            System.Diagnostics.Debug.WriteLine("Statement Lambda!");
+            System.Diagnostics.Debug.WriteLine("Statement Lambda!");
+            System.Diagnostics.Debug.WriteLine("Statement Lambda!");
+        };
+        statement();
+    }
+}
