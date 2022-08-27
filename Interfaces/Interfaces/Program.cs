5e87404cac0dae81eeaa1a6cb1370ac6cf23e34c
Souvik Kundu <85146980+souvikk27@users.noreply.github.com>
2022-08-27 15:45:55 +0530

    Add files via upload

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+interface Example{
+    void ex();
+    int ex1();
+    string ex2(string str);
+}
+class Solution : Example
+{
+    public static void Main(string[] args)
+    {
+        var obj = new Solution();
+        obj.ex1();
+        int x = obj.ex1();
+        Console.WriteLine(x);
+        string y = obj.ex2("Souvik");
+        Console.WriteLine(y);
+        obj.ex();
+
+    }
+
+    public void ex()
+    {
+        Console.WriteLine("Void Function!");
+    }
+
+    public int ex1()
+    {
+        return 1;
+    }
+
+    public string ex2(string str)
+    {
+        return str;
+    }
+}
